08d3a13802824e56ac5a7f7c36f7c192d511f3f4
Carson Jones <carsoncjones@gmail.com>
2026-03-14 22:40:20 -0500

    comments and tmux

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -4,6 +4,7 @@ return {
   config = function()
     require('mini.ai').setup { n_lines = 500 }
     require('mini.surround').setup()
+    require('mini.comment').setup()
 
     local statusline = require 'mini.statusline'
     statusline.setup {
