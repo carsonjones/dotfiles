e70b88851a26c34c2abc29f801ffeefb24468842
Carson Jones <carsoncjones@gmail.com>
2026-03-18 10:12:27 -0500

    oops, backwards

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>?', function() require('which-key').show() end, { desc = 'Show keymaps' })
+vim.keymap.set('n', '<leader>?', function()
+  require('which-key').show()
+end, { desc = 'Show keymaps' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = 'Show diagnostic float' })
 
@@ -16,39 +18,41 @@ vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w><C-h>', { desc = 'Move focus left 
 vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w><C-l>', { desc = 'Move focus right from terminal' })
 
 vim.keymap.set('n', '<leader>yn', function()
-  local name = vim.fn.expand('%:t')
+  local name = vim.fn.expand '%:t'
   vim.fn.setreg('+', name)
   vim.notify('Copied: ' .. name)
 end, { desc = 'Yank filename' })
 
 vim.keymap.set('n', '<leader>yp', function()
-  local path = vim.fn.expand('%:.')
+  local path = vim.fn.expand '%:.'
   vim.fn.setreg('+', path)
   vim.notify('Copied: ' .. path)
 end, { desc = 'Yank relative path' })
 
 vim.keymap.set('n', '<leader>yd', function()
-  local diags = vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 })
+  local diags = vim.diagnostic.get(0, { lnum = vim.fn.line '.' - 1 })
   if #diags == 0 then
-    vim.notify('No diagnostics on current line')
+    vim.notify 'No diagnostics on current line'
     return
   end
-  local lines = vim.tbl_map(function(d) return d.message end, diags)
+  local lines = vim.tbl_map(function(d)
+    return d.message
+  end, diags)
   local text = table.concat(lines, '\n')
   vim.fn.setreg('+', text)
   vim.notify('Copied ' .. #diags .. ' diagnostic(s)')
 end, { desc = 'Yank diagnostics (line)' })
 
 -- Move lines up/down (Option+j/k)
-vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down' })
-vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' })
-vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
-vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+vim.keymap.set('n', '<A-k>', ':m .+1<CR>==', { desc = 'Move line down' })
+vim.keymap.set('n', '<A-j>', ':m .-2<CR>==', { desc = 'Move line up' })
+vim.keymap.set('v', '<A-k>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set('v', '<A-j>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
 
 vim.keymap.set('n', '<leader>yD', function()
   local diags = vim.diagnostic.get(0)
   if #diags == 0 then
-    vim.notify('No diagnostics in buffer')
+    vim.notify 'No diagnostics in buffer'
     return
   end
   local lines = vim.tbl_map(function(d)
