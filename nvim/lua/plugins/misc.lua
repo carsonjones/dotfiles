d5c0aeba9c1fae69f316bdcdec11c24d1a853390
Carson Jones <carsoncjones@gmail.com>
2026-04-20 19:44:26 -0500

    adjust

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -2,7 +2,13 @@ return {
   {
     'leath-dub/snipe.nvim',
     keys = {
-      { 'gb', function() require('snipe').open_buffer_menu() end, desc = 'Snipe buffer menu' },
+      {
+        'gb',
+        function()
+          require('snipe').open_buffer_menu()
+        end,
+        desc = 'Snipe buffer menu',
+      },
     },
     opts = {
       ui = { position = 'topleft', border = 'rounded', max_path_width = 1 },
@@ -13,8 +19,21 @@ return {
     'MagicDuck/grug-far.nvim',
     cmd = 'GrugFar',
     keys = {
-      { '<leader>fr', function() require('grug-far').open { prefills = { search = vim.fn.expand '<cword>' } } end, desc = 'Find & replace' },
-      { '<leader>fr', function() require('grug-far').with_visual_selection() end, mode = 'v', desc = 'Find & replace selection' },
+      {
+        '<leader>fr',
+        function()
+          require('grug-far').open { prefills = { search = vim.fn.expand '<cword>' } }
+        end,
+        desc = 'Find & replace',
+      },
+      {
+        '<leader>fr',
+        function()
+          require('grug-far').with_visual_selection()
+        end,
+        mode = 'v',
+        desc = 'Find & replace selection',
+      },
     },
     opts = {},
   },
@@ -23,12 +42,49 @@ return {
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
-      { '<leader>a', function() require('harpoon'):list():add() end, desc = 'Harpoon add' },
-      { '<C-e>', function() local h = require('harpoon') h.ui:toggle_quick_menu(h:list()) end, desc = 'Harpoon menu' },
-      { '<leader>1', function() require('harpoon'):list():select(1) end, desc = 'Harpoon 1' },
-      { '<leader>2', function() require('harpoon'):list():select(2) end, desc = 'Harpoon 2' },
-      { '<leader>3', function() require('harpoon'):list():select(3) end, desc = 'Harpoon 3' },
-      { '<leader>4', function() require('harpoon'):list():select(4) end, desc = 'Harpoon 4' },
+      {
+        '<leader>a',
+        function()
+          require('harpoon'):list():add()
+        end,
+        desc = 'Harpoon add',
+      },
+      {
+        '<C-e>',
+        function()
+          local h = require 'harpoon'
+          h.ui:toggle_quick_menu(h:list())
+        end,
+        desc = 'Harpoon menu',
+      },
+      {
+        '<leader>1',
+        function()
+          require('harpoon'):list():select(1)
+        end,
+        desc = 'Harpoon 1',
+      },
+      {
+        '<leader>2',
+        function()
+          require('harpoon'):list():select(2)
+        end,
+        desc = 'Harpoon 2',
+      },
+      {
+        '<leader>3',
+        function()
+          require('harpoon'):list():select(3)
+        end,
+        desc = 'Harpoon 3',
+      },
+      {
+        '<leader>4',
+        function()
+          require('harpoon'):list():select(4)
+        end,
+        desc = 'Harpoon 4',
+      },
     },
     opts = {},
   },
@@ -43,11 +99,46 @@ return {
     event = 'VeryLazy',
     opts = {},
     keys = {
-      { 's', mode = { 'n', 'x' }, function() require('flash').jump() end, desc = 'Flash' },
-      { 'S', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter' },
-      { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote Flash' },
-      { 'R', mode = { 'o', 'x' }, function() require('flash').treesitter_search() end, desc = 'Treesitter Search' },
-      { '<c-s>', mode = { 'c' }, function() require('flash').toggle() end, desc = 'Toggle Flash Search' },
+      {
+        'gs',
+        mode = { 'n', 'x' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+      {
+        'gS',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').treesitter()
+        end,
+        desc = 'Flash Treesitter',
+      },
+      {
+        'r',
+        mode = 'o',
+        function()
+          require('flash').remote()
+        end,
+        desc = 'Remote Flash',
+      },
+      {
+        'R',
+        mode = { 'o', 'x' },
+        function()
+          require('flash').treesitter_search()
+        end,
+        desc = 'Treesitter Search',
+      },
+      {
+        '<c-s>',
+        mode = { 'c' },
+        function()
+          require('flash').toggle()
+        end,
+        desc = 'Toggle Flash Search',
+      },
     },
   },
 }
