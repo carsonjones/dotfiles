e8883a8190f9569b8604d16c349956b482b554d7
Carson Jones <carsoncjones@gmail.com>
2026-06-24 05:54:40 +0000

    nvim: skip TailwindSort when LSP not attached
    avoids 'tailwind-language-server is not running' spam on save in non-tailwind projects

--- nvim/lua/plugins/tailwind-tools.lua
+++ nvim/lua/plugins/tailwind-tools.lua
@@ -16,8 +16,12 @@ return {
     require('tailwind-tools').setup(opts)
     vim.api.nvim_create_autocmd('BufWritePre', {
       pattern = { '*.html', '*.css', '*.js', '*.ts', '*.jsx', '*.tsx', '*.astro' },
-      callback = function()
-        vim.cmd 'TailwindSortSync'
+      callback = function(args)
+        -- only sort when the tailwind LSP is actually attached, else
+        -- non-tailwind projects spam "tailwind-language-server is not running"
+        if next(vim.lsp.get_clients { bufnr = args.buf, name = 'tailwindcss' }) then
+          vim.cmd 'TailwindSortSync'
+        end
       end,
     })
   end,
